629fc806c3df29a8567eafecbb20997d8e7d647a
Ayush Khandelwal <ayushkhandelwal1974@gmail.com>
2025-12-17 09:33:35 +0530

    added some more unique features

--- components/CustomCursor.jsx
+++ components/CustomCursor.jsx
@@ -1,23 +1,38 @@
 import { useEffect, useState } from "react";
 
 export default function CustomCursor() {
-  const [pos, setPos] = useState({ x: 0, y: 0 });
+  const [mouse, setMouse] = useState({ x: 0, y: 0 });
+  const [follower, setFollower] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const move = (e) => {
-      setPos({ x: e.clientX, y: e.clientY });
+      setMouse({ x: e.clientX, y: e.clientY });
     };
     window.addEventListener("mousemove", move);
     return () => window.removeEventListener("mousemove", move);
   }, []);
 
+  useEffect(() => {
+    const follow = () => {
+      setFollower((prev) => ({
+        x: prev.x + (mouse.x - prev.x) * 0.15,
+        y: prev.y + (mouse.y - prev.y) * 0.15,
+      }));
+      requestAnimationFrame(follow);
+    };
+    follow();
+  }, [mouse]);
+
   return (
-    <div
-      className="custom-cursor"
-      style={{
-        left: pos.x,
-        top: pos.y,
-      }}
-    />
+    <>
+      <div
+        className="custom-cursor"
+        style={{ left: mouse.x, top: mouse.y }}
+      />
+      <div
+        className="cursor-follower"
+        style={{ left: follower.x, top: follower.y }}
+      />
+    </>
   );
 }
